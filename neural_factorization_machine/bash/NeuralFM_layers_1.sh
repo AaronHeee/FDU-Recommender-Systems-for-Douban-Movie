e988de44756f6a8965906d4cafc8d9658e8e3e73
AaronHeee <aaronheee@gmail.com>
2018-05-28 17:12:43 +0800

    new tasks

--- neural_factorization_machine/bash/NeuralFM_layers_1.sh
+++
@@ -1,4 +0,0 @@
-for h in 8 16 32 64 128 256 512
-do
-    CUDA_VISIBLE_DEVICES="0" python NeuralFM.py --epoch 120 --batch_size 32 --lr 0.01 --hidden_factor $h --layers [$h] --log_path zhankui --log_on [\'hidden_factor\',\'layers\'];
-done
